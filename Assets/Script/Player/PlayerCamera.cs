667e9509182ea57f349d2206b3ab5130b5c2fa03
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-07-13 19:25:46 +0900

    Player Camera 2

--- Assets/Script/Player/PlayerCamera.cs
+++ Assets/Script/Player/PlayerCamera.cs
@@ -7,11 +7,26 @@ public class PlayerCamera
 {
     GameObject current;    
     [SerializeField] GameObject camera;
+    public float xRotate = 0;
     public void SetPlayer(GameObject gameObject)
     {
         current = gameObject;
     }
     public void Update() 
+    {
+        CameraMoving();
+        MouseRotation();
+    }
+    private void MouseRotation()
+    {
+        float yRotateSize = Input.GetAxis("Mouse X");
+        float yRotate = camera.transform.eulerAngles.y + yRotateSize;
+        float xRotateSize = Input.GetAxis("Mouse Y");
+        xRotate = Mathf.Clamp(xRotate + xRotateSize, -45, 80);
+        camera.transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
+
+    }
+    private void CameraMoving()
     {
         camera.transform.position = current.transform.position + new Vector3(0, 10, 0);
     }
